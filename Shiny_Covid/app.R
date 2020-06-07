b46f464bf6e54076110b33e34c3dcb17b2868a14
Alfredo Jose Alves Rodrigues Neto <alfredojoseneto@gmail.com>
2020-06-07 20:45:58 -0300

    Add plot of linear deaths in the app.R

--- Shiny_Covid/app.R
+++ Shiny_Covid/app.R
@@ -19,6 +19,7 @@ df_covid <- read.csv(file = "./input/caso_full.csv.gz", sep = ",", dec = ",",
                      encoding = "UTF-8", stringsAsFactors = FALSE)
 
 df_covid$date <- as.Date(df_covid$date)
+
 # Data wrangling ----------------------------------------------------------
 
 # Converting empty string in city variable to NA
@@ -51,7 +52,6 @@ df_covid_shiny <- df_covid_abc %>%
             new_deaths) %>% 
   mutate(cum_sum_daily_cases = cumsum(new_confirmed),
          cum_sum_daily_deaths = cumsum(new_deaths))
-cumsum(df_covid_abc$new)
 
 # Shiny App ---------------------------------------------------------------
 
@@ -87,7 +87,8 @@ ui <- fluidPage(
       ) #end of checkboxGroupInput
     ), #end of sidebarPanel
     
-    mainPanel(plotOutput("covid_linear")) #end of mainPanel
+    mainPanel(plotOutput("covid_linear"),
+              plotOutput("covid_linear_death")) #end of mainPanel
   ), #end of sidebarLayout
   
   tags$hr(),
@@ -124,7 +125,7 @@ server <- function(input, output) {
                  linetype = "dashed",
                  colour = "#2769db") +
       scale_color_discrete(guide = "none") +
-      scale_x_date(expand = expansion(add = c(1, 12)),
+      scale_x_date(expand = expansion(mult = c(0.01, 0.25)),
                    limits = c(first_day, last_day),
                    breaks = seq(from = first_day, to = last_day, by = 3),
                    date_labels = "%d-%B") +
@@ -154,8 +155,55 @@ server <- function(input, output) {
     
     print(plot_shiny)
     
-  })
-}
+  }) #end of renderPlot --> output$linear_covid
+  
+  output$covid_linear_death <- renderPlot({
+    
+    
+    # Limits of days of the scale_x_date()
+    first_day = min(df_covid_shiny$date)
+    last_day = max(df_covid_shiny$date)
+    
+    # Setting colours of the cities
+    main_color = "#5d615e"
+    
+    # Position of the annotation about quarentine
+    x_quarentine_pos <- as.Date("2020-03-25")
+    y_quarentine_pos <- max(df_covid_shiny$cum_sum_daily_cases)
+    
+    
+    plot_shiny_deaths <- df_covid_shiny %>%
+      filter(date >= paste(input$date_range[1]),
+             date <= paste(input$date_range[2]),
+             city %in% input$cities_abc) %>% 
+      ggplot(data = .,
+             mapping = aes(x = date, y = cum_sum_daily_deaths, colour = city)) +
+      geom_line(size = 1) +
+      scale_x_date(expand = expansion(mult = c(0.01, 0.25)),
+                   breaks = seq(from = first_day, to = last_day, by = 3),
+                   limits = c(first_day, last_day),
+                   date_labels = "%d-%B") +
+      scale_color_discrete(guide = "none") +
+      directlabels::geom_dl(mapping = aes(label = city),
+                            method = list(dl.trans(x = x + 0.2), "last.points")) +
+      labs(title = "Evolução do número de óbitos por COVID acumulados por dia nas 7 cidades do ABC",
+           x = "Data dos novos óbitos",
+           y = "Número de óbitos acumulados por dia (linear)",
+           caption = "Dados públicos e abertos providos pelo Brasil.IO") +
+      theme_bw() +
+      theme(
+        plot.title = element_text(size = 20, colour = main_color),
+        plot.subtitle = element_text(colour = main_color),
+        plot.caption = element_text(colour = main_color),
+        axis.title.x = element_text(size = 14, colour = main_color),
+        axis.text.x = element_text(size = 12, angle = 45, hjust = 1, vjust = 1, colour = main_color),
+        axis.title.y = element_text(size = 14, colour = main_color),
+        axis.text.y = element_text(size = 12, colour = main_color)
+      )
+    print(plot_shiny_deaths)
+  }) #end of renderPlot --> output$covid_linear_death
+  
+} #end of server function
 
 
 shinyApp(ui = ui, server = server)

076f77f6c862ff799c74ea70fc6fbcfa00d510e1
Alfredo Jose Alves Rodrigues Neto <alfredojoseneto@gmail.com>
2020-06-11 20:56:39 -0300

    Add R tip 04 and organize files into folder

---
+++ R_Tips/04_Fill_gaps/04_Fill_gaps.R
@@ -0,0 +1,20 @@
+# Libraries ---------------------------------------------------------------
+library(tidyverse)
+library(readxl)
+library(rstudioapi)
+
+
+# Setting working directory -----------------------------------------------
+dirname(rstudioapi::getActiveDocumentContext()$path) %>% setwd()
+
+
+# Loading data ------------------------------------------------------------
+df_test <- readxl::read_xlsx(path = "Teste.xlsx")
+
+
+# Fill the NAs gaps -------------------------------------------------------
+df_test %>% 
+  fill(AGRUPAMENTO_1,
+       AGRUPAMENTO_2) %>% 
+  view()
+
